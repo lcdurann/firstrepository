588003fdc512e826b95a117e881a458153c8b995
Luis C. Duran <lcdurann@unal.edu.co>
2017-08-04 19:34:25 -0700

    second file

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+  int x,y,z;
+  cout<< "x=";
+  cin>>x;
+  cout<< "y=";
+  cin>>y;
+  z=x*x+y*y;
+  cout<<"z=";
+  cout<<z<<endl;
+  return 0;
+}
+  
+  
